986d950a5090dcdb9e20a953f4dde3974390f984
Akshada <akshadachaure5@gmail.com>
2023-07-05 15:59:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>Hello World</h1>'
 })
 export class AppComponent {
   title = 'AngularProject';
